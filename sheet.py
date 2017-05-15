c9fc896971697626a0e5e320a2aa707b124adedc
Kamal Mohanan <kamalmohanan2.71@gmail.com>
2017-05-15 19:11:16 +0530

    buggy version

--- sheet.py
+++ sheet.py
@@ -1,16 +1,34 @@
 #!/usr/bin/env python3
 import inspect
 import pdb
+import re
+import sys
 
 class Cell:
-    def __init__(self, val=None, expr=None ):
-        self.val = val
-        self.expr = expr
+    def __init__(self):
+        self.val = None
+        self.expr = None
         self.children = []
         self.parents = []
     
     def __repr__(self):
         return "*".join([str(x) for x in (self.val, self.expr, self.children, self.parents)])
+
+class Row:
+    def __init__(self):
+        self.expr = None
+        self.children = []
+        self.parents = []
+
+class Column:
+    def __init__(self):
+        self.expr = None
+        self.children = []
+        self.parents = []
+
+def get_row_col(id):
+    match = re.search(r'^([a-zA-Z]+)([0-9]+)$', id)
+    return (match.group(1), match.group(2))
 
 def create_sheet():
     return {}
@@ -22,77 +40,143 @@ def get_cell(sheet, cell_id):
         sheet[cell_id] = cell
     return cell
 
+def get_row(sheet, id):
+    row = sheet.get(id) 
+    if not row:
+        row = Row()
+        sheet[id] = row
+    return row
 
-def put(sheet, cell_id, value):
-    cell = get_cell(sheet, cell_id)
+def get_col(sheet, id):
+    col = sheet.get(id) 
+    if not col:
+        col = Column()
+        sheet[id] = col
+    return col
 
-    for parent_id in cell.parents:
-        parent_cell = get_cell(sheet, parent_id)
-        parent_cell.children.remove(cell_id)
-    # old parents' children list is now ok.
-
-    if callable(value):
-        
-        new_parents = list(inspect.signature(value).parameters)
-
-        cell.parents = new_parents
-
-        for parent_id in new_parents:
-            parent_cell = get_cell(sheet, parent_id)
-            parent_cell.children.append(cell_id)
-
+def get_type(id):
+    if re.search(r'^[a-zA-Z]+$', id):
+        return 'Column'
+    elif re.search(r'^[0-9]+$', id):
+        return 'Row'
+    elif re.search(r'^([a-zA-Z]+)([0-9]+)$', id):
+        return 'Cell'
     else:
-        cell.parents = []
+        return None
 
-    cell.expr = value
-    evaluate(sheet, cell)
-    
-    return cell
+
+
+def put(sheet, place, value):
+    try:
+        cell = get_cell(sheet, place)
+
+        row_id, col_id = get_row_col(place) # Raises exception
+
+        for parent_id in cell.parents:
+            t = get_type(parent_id)
+            if t == 'Cell':
+                parent = get_cell(sheet, parent_id)
+            elif t == 'Row':
+                parent = get_row(sheet, parent_id)
+            elif t == 'Col':
+                parent = get_col(sheet, parent_id)
+            else:
+                raise Exception
+            parent.children.remove(place)
+                
+        # old parents' children list is now ok.
+
+        if callable(value):
+            
+            new_parents = list(inspect.signature(value).parameters)
+
+            cell.parents = new_parents
+
+            for parent_id in new_parents:
+                t = get_type(parent_id)
+                if t == 'Cell':
+                    parent = get_cell(sheet, parent_id)
+                elif t == 'Row':
+                    parent = get_row(sheet, parent_id)
+                elif t == 'Column':
+                    parent = get_col(sheet, parent_id)
+                else:
+                    raise Exception
+                parent.children.append(place)
+
+
+        else:
+            cell.parents = []
+
+        cell.expr = value
+        evaluate(sheet, cell)
+
+        return cell
+    except:
+        print(place + ' is not a valid cell.', file=sys.stderr)
+        return None
+
+def get(sheet, cell_id):
+    return get_cell(sheet, cell_id).val
 
 def evaluate(sheet, cell):
     if not callable(cell.expr):
         cell.val = cell.expr
     else:
-        args = [get_cell(sheet, parent_id).val for parent_id in cell.parents]
-        cell.val = cell.expr(*args)
+        # args = [get_cell(sheet, parent_id).val for parent_id in cell.parents]
+        # cell.val = cell.expr(*args)
+        args = []
+        for parent_id in cell.parents:
+            t = get_type(parent_id)
+            if t == "Cell":
+                args.append(get_cell(sheet, parent_id).val)
+            elif t == "Row":
+                cells_in_row = get_cells_in_row(sheet, parent_id)
+                args.extend(cells_in_row)
+            elif t == "Column":
+                cells_in_col = get_cells_in_col(sheet, parent_id)
+                args.extend(cells_in_col)
+            else:
+                raise Exception
+
+        try:
+            cell.val = cell.expr(*args)
+        except:
+            cell.val = cell.expr(args)
+
+
+
+
     for child_id in cell.children:
         evaluate(sheet, get_cell(sheet, child_id))
 
-            
+def get_cells_in_col(sheet, col_id):
+    cell_ids = []
+    pattern = r'^%s[0-9]+$' % col_id
+    for id in sheet.keys():
+        if re.search(pattern, id):
+            cell_ids.append(id)
+    return [get_cell(sheet, id).val for id in cell_ids]
 
-def foo(a, b):
-    return a+b
+def get_cells_in_row(sheet, row_id):
+    cell_ids = []
+    pattern = r'^[a-zA-Z]+%s$' % row_id
+    for id in sheet.keys():
+        if re.search(pattern, id):
+            cell_ids.append(id)
+    return [get_cell(sheet, id).val for id in cell_ids]
 
-def id(c):
-    return c
 
-def bar(a):
-    return a+1
-
-def baz(b):
-    return b+1
 
 if __name__ == '__main__':
 
     s = create_sheet()
 
-
-    put(s, 'a', 1)
-    put(s, 'b', 2)
-    put(s, 'c', foo)
-
-    put(s, 'a', 5)
-
-    put(s, 'd', id)
-
-    put(s, 'b', 6)
-    
+    put(s, 'a1', 1)
+    put(s, 'a2', 2)
+    put(s, 'a3', 3)
+    put(s, 'a4', 4)
     pdb.set_trace()
-    # b a c d
-    put(s, 'c', bar)
-    
-    put(s, 'a', baz)
-
-    print("hello da")
+    put(s, 'b1', lambda a: sum(a))
 
 
